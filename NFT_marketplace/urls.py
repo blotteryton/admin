9ebf7ff406d0dc24ce2df9b49581532c6fb579d6
truezed <metruezed@gmail.com>
2022-10-31 20:35:45 +0300

    Updated urls.

--- NFT_marketplace/urls.py
+++ NFT_marketplace/urls.py
@@ -1,31 +1,19 @@
-"""NFT_marketplace URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
-
+from django.utils.translation import gettext_lazy
 
 from NFT_marketplace import settings
 
+
+admin.site.site_header = 'Blottery'  # default: "Django Administration"
+admin.site.index_title = gettext_lazy('Personal Area')  # default: "Site administration"
+admin.site.site_title = 'Blottery'  # default: "Django site admin"
+
 urlpatterns = [
-    path('login/', admin.site.urls),
-    #path('register/', TemplateView.as_view(template_name='register.html'), name='register'),
-    path('register/', include('users.urls'))
+    path('', include("users.urls")),
+    path('', admin.site.urls, name="account"),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 
 from users.views import Register, CreateWallet
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    path('', Register.as_view(), name='register'),
+    path('register/', Register.as_view(), name='register'),
     path('create_wallet/', CreateWallet.as_view(), name='create_wallet')
 ]
