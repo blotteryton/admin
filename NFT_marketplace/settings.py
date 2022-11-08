79157c9f09dfa4ed96128a503ad2e65dbe3090b3
truezed <metruezed@gmail.com>
2022-11-08 23:05:10 +0300

    requirements.txt and settings.py updated.

--- NFT_marketplace/settings.py
+++ NFT_marketplace/settings.py
@@ -99,8 +99,7 @@ REST_FRAMEWORK = {
             'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'PAGE_SIZE': 10
 }
 
 
